75574de82e00f83d228b1a6cbb87808617f2d7bc
Ewen35550 <ewen.herve.35550@gmail.com>
2023-01-14 16:19:30 +0100

    Ajout de cross_origin pour autoriser l'appel de l'api

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_cors import CORS, cross_origin
+from flask_cors import cross_origin
 from markupsafe import escape
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -14,48 +14,57 @@ def index():
 # ==================== Spotipy ====================
 # Album
 @app.get('/album/<string:id>')
+@cross_origin()
 def get_album(id):
     return sp.album(escape(id))
 
 # Album with tracks
 @app.get('/album/<string:id>/tracks')
+@cross_origin()
 def get_album_tracks(id):
     return sp.album_tracks(escape(id))
 
 # Artist
 @app.get('/artist/<string:id>')
+@cross_origin()
 def get_artist(id):
     return sp.artist(escape(id))
 
 # Artist with albums
 @app.get('/artist/<string:id>/albums')
+@cross_origin()
 def get_artist_albums(id):
     return sp.artist_albums(escape(id))
 
 # Artist top tracks
 @app.get('/artist/<string:id>/top-tracks')
+@cross_origin()
 def get_artist_top_tracks(id):
     return sp.artist_top_tracks(escape(id))
 
 # Track
 @app.get('/track/<string:id>')
+@cross_origin()
 def get_track(id):
     return sp.track(escape(id))
 
 # Search album
 @app.get('/search/album/<string:name>', defaults={'limit': 10})
 @app.get('/search/album/<string:name>/<int:limit>')
+@cross_origin()
 def search_album(name, limit):
     return sp.search(q='album:' + escape(name), type='album', limit=escape(limit))
 
 # Search artist
 @app.get('/search/artist/<string:name>', defaults={'limit': 5})
 @app.get('/search/artist/<string:name>/<int:limit>')
+@cross_origin()
 def search_artist(name, limit):
     return sp.search(q='artist:' + escape(name), type='artist', limit=escape(limit))
 
 # Search track
 @app.get('/search/track/<string:name>', defaults={'limit': 10})
 @app.get('/search/track/<string:name>/<int:limit>')
+@cross_origin()
 def search_track(name, limit):
     return sp.search(q='track:' + escape(name), type='track', limit=escape(limit))
